2703899ec5dcf4f3fda2ced44a6b479197c59d7e
Brycew30 <brycew30@gmail.com>
2020-07-16 12:42:59 -0500

    update team_api filename

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ Bundler.require
 
 require_relative '../lib/services/version.rb'
 require_relative '../lib/services/cli.rb'
-require_relative '../lib/services/nhl_api.rb'
+require_relative '../lib/services/team_api.rb'
 require_relative '../lib/models/team.rb'
