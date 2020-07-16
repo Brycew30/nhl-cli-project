0e8e9b382a4e79326aa4e21df12b0140a5a06e86
Brycew30 <brycew30@gmail.com>
2020-07-16 12:11:00 -0500

    change model name to team

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ Bundler.require
 require_relative '../lib/services/version.rb'
 require_relative '../lib/services/cli.rb'
 require_relative '../lib/services/nhl_api.rb'
-require_relative '../lib/models/nhl.rb'
+require_relative '../lib/models/team.rb'
