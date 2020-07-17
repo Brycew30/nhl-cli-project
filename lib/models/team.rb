8c1f6d50d99d4b18068d9913bd1038c806438d16
Brycew30 <brycew30@gmail.com>
2020-07-17 09:07:54 -0500

    create send method for initialize

--- lib/models/team.rb
+++ lib/models/team.rb
@@ -1,16 +1,17 @@
 class Nhl::Team #model class
-  attr_reader :name, :abbreviation, :arena, :division, :conference, :first_year, :url
+  attr_accessor :name, :abbreviation, :arena, :division, :conference, :first_year, :url
 
   @@all = []
 
   def initialize(attr_hash)
-    @name = attr_hash[:name]
-    @abbreviation = attr_hash[:abbreviation]
-    @arena = attr_hash[:arena]
-    @division = attr_hash[:division]
-    @conference = attr_hash[:conference]
-    @first_year = attr_hash[:first_year]
-    @url = attr_hash[:url]
+    attr_hash.each {|k, v| self.send("#{k}=",v)}
+    # @name = attr_hash[:name]
+    # @abbreviation = attr_hash[:abbreviation]
+    # @arena = attr_hash[:arena]
+    # @division = attr_hash[:division]
+    # @conference = attr_hash[:conference]
+    # @first_year = attr_hash[:first_year]
+    # @url = attr_hash[:url]
     save
   end
 
